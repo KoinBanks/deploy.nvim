0ab6aefa6da5ff245e75d00f7ace50790e1c7bd1
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-04-16 23:08:33 +0200

    feat: add LogLevel=ERROR to ssh args

--- lua/deploy/utils.lua
+++ lua/deploy/utils.lua
@@ -61,11 +61,12 @@ end
 
 ---@param port integer
 ---@param return_string boolean
-M.build_ssh_options = function(port, return_string)
+M.build_ssh_flags = function(port, return_string)
   -- no host checking and no password auth for better security and to avoid hanging if the host is not reachable
   local options = {
     "-o PasswordAuthentication=no",
     "-o StrictHostKeyChecking=no",
+    "-o LogLevel=ERROR",
     "-p " .. tostring(port),
   }
 
